16233a3917fc4f9a48990d4202b8ca6c5377cbfe
sayed saud <89771866+sayedsaud@users.noreply.github.com>
2024-05-14 18:46:13 +0530

    'update'

--- first-day.py
+++ first-day.py
@@ -1,2 +1,8 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+a=24
+print(a)
+
+print(id(a))
+
+b=24
+print(id(b))
+
