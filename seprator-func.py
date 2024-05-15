e624b2b588be72c9620da8fabb68e55dc8378c67
sayed saud <89771866+sayedsaud@users.noreply.github.com>
2024-05-15 18:54:54 +0530

    'function'

---
+++ seprator-func.py
@@ -0,0 +1,13 @@
+print("*"*10)
+print("#",end=" ")
+print("#",end=" ")
+print("#")
+
+print("hello saud","how are you","where do you stay ? ",sep="--")
+
+print(11&8)
+
+print(~10)
+
+print(20^30)
+
