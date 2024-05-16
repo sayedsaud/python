565c06264c50e78e69f2cbb36b9458f92f192761
sayed saud <89771866+sayedsaud@users.noreply.github.com>
2024-05-16 17:32:25 +0530

    loop-function

---
+++ loop-function.py
@@ -0,0 +1,15 @@
+if (1 == 1):
+    print(True)
+else:
+    print(False)
+
+
+a=1
+b=10
+c=0
+if a>b and a>c:
+   print(a,"is greater")
+elif b>a and b>c:
+   print(b,"is greater")       
+else:
+    print(c,"is greater")
